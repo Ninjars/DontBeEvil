95dcff240616f33a507a1764b5f1dd3b5f6634c4
Jez <jez0707@gmail.com>
2016-03-25 22:28:12 +0000

    Had IncrementableValue's fields as static for some silly reason! Decided the the increment amount isn't currently expected to be observable, so simplified things by making it a long.

--- app/src/main/java/com/projects/jez/dontbeevil/data/IncrementableValue.java
+++ app/src/main/java/com/projects/jez/dontbeevil/data/IncrementableValue.java
@@ -7,18 +7,18 @@ import com.projects.jez.utils.observable.Source;
  * Created by Jez on 18/03/2016.
  */
 public class IncrementableValue {
-    private static final Source<Long> mCount = new Source<>(0l);
-    private static final Source<Long> mAmountPerTick = new Source<>(1l);
+    private final Source<Long> mCount = new Source<>(0l);
+    private long mAmountPerTick = 1l;
 
     public Observable<Long> getValue() {
         return mCount.getObservable();
     }
 
     public void increment() {
-        mCount.put(mCount.getObservable().getCurrent() + mAmountPerTick.getObservable().getCurrent());
+        mCount.put(mCount.getObservable().getCurrent() + mAmountPerTick);
     }
 
     public void setAmountPerIncrement(long tickIncrement) {
-        mAmountPerTick.put(tickIncrement);
+        mAmountPerTick = tickIncrement;
     }
 }
