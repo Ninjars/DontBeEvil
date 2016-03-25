6a568cea5f76512c286dfa78b9c0c092d7be354b
Jez <jez0707@gmail.com>
2016-03-25 17:19:53 +0000

    GameState now displays a full list of IncrementerReadouts.

--- app/src/main/java/com/projects/jez/dontbeevil/state/GameState.java
+++ app/src/main/java/com/projects/jez/dontbeevil/state/GameState.java
@@ -1,6 +1,8 @@
 package com.projects.jez.dontbeevil.state;
 
-import com.projects.jez.dontbeevil.Constants;
+import android.util.Log;
+
+import com.projects.jez.dontbeevil.data.Incrementer;
 import com.projects.jez.dontbeevil.managers.IncrementerManager;
 import com.projects.jez.utils.observable.ObservableList.ObservableArrayList;
 import com.projects.jez.utils.observable.ObservableList.ObservableList;
@@ -9,10 +11,16 @@ import com.projects.jez.utils.observable.ObservableList.ObservableList;
  * Created by Jez on 18/03/2016.
  */
 public class GameState {
+    private static final String TAG = GameState.class.getSimpleName();
+    private static final boolean DLOG = true;
+
     private final ObservableArrayList<IncrementerReadout> readouts = new ObservableArrayList<>();
 
     public GameState(IncrementerManager incManager) {
-        readouts.add(incManager.getIncrementer(Constants.cPlayIncrementerId));
+        for (Incrementer readout : incManager.getAllIncrementers()) {
+            if (DLOG) Log.d(TAG, "adding readout for " + readout.getId());
+            readouts.add(readout);
+        }
     }
 
     public ObservableList<IncrementerReadout> getReadouts() {
