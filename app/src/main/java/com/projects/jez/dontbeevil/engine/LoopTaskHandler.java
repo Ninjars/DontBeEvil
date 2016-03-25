f02e5a82b5968d2ef8272050af7f0596ac1f9213
Jez <jez0707@gmail.com>
2016-03-25 14:08:12 +0000

    Added Range to LoopTaskHandler, which can be queried to get a completion progress value. Range is exposed through an observable, and updated on each loop. The UI can observe for Range changes then poll the value with System.currentTimeMillis() to get a progress fraction. Added LoopTaskHandler.setPeriod() Changed LoopTaskHandler.resume() to ignore resume calls if it's not been stopped, and to treat it as a start call if it's not already been started.

--- app/src/main/java/com/projects/jez/dontbeevil/engine/LoopTaskHandler.java
+++ app/src/main/java/com/projects/jez/dontbeevil/engine/LoopTaskHandler.java
@@ -3,14 +3,18 @@ package com.projects.jez.dontbeevil.engine;
 import android.os.Handler;
 import android.os.Looper;
 
+import com.projects.jez.utils.Box;
+import com.projects.jez.utils.observable.Observable;
+import com.projects.jez.utils.observable.Source;
+
 /**
  * Created by jez on 22/03/2016.
  */
 public class LoopTaskHandler {
 
     private final Handler cLoopHandler = new Handler(Looper.getMainLooper());
+    private final Source<Box<Range>> mRange = new Source<>(new Box<Range>(null));
     private final Runnable mTask;
-
     private Long lastUpdateStarted;
     private Long lastUpdateStopped;
     private long mLoopPeriod;
@@ -27,22 +31,48 @@ public class LoopTaskHandler {
     }
 
     public void start() {
+        lastUpdateStopped = null;
         update();
     }
 
     private void update() {
         lastUpdateStarted = System.currentTimeMillis();
+        mRange.put(new Box<>(new Range(lastUpdateStarted, mLoopPeriod)));
         cLoopHandler.postDelayed(mTask, mLoopPeriod);
     }
 
     public void stop() {
         lastUpdateStopped = System.currentTimeMillis();
+        mRange.put(new Box<Range>(null));
         cLoopHandler.removeCallbacksAndMessages(null);
     }
 
     public void resume() {
-        if (lastUpdateStarted == null || lastUpdateStopped == null) update();
-        long delta = Math.min(Math.max(0, mLoopPeriod - (lastUpdateStopped - lastUpdateStarted)), mLoopPeriod);
-        cLoopHandler.postDelayed(mTask, delta);
+        if (lastUpdateStopped != null) {
+            // does not appear to have been stopped, ignore resume
+            //noinspection UnnecessaryReturnStatement
+            return;
+
+        } else if (lastUpdateStarted == null) {
+            // does not appear to have been started, so start
+            start();
+
+        } else {
+            // resuming accounting for previous progress
+            lastUpdateStopped = null;
+            long delta = Math.min(Math.max(0, mLoopPeriod - (lastUpdateStopped - lastUpdateStarted)), mLoopPeriod);
+            mRange.put(new Box<>(new Range(System.currentTimeMillis() - (mLoopPeriod - delta), mLoopPeriod)));
+            cLoopHandler.postDelayed(mTask, delta);
+        }
+    }
+
+    public void setPeriod(long period) {
+        mLoopPeriod = period;
+        stop();
+        resume();
+    }
+
+    public Observable<Box<Range>> getRangeObservable() {
+        return mRange.getObservable();
     }
 }
---
+++ app/src/main/java/com/projects/jez/dontbeevil/engine/Range.java
@@ -0,0 +1,22 @@
+package com.projects.jez.dontbeevil.engine;
+
+import com.projects.jez.dontbeevil.BuildConfig;
+
+/**
+ * Created by Jez on 25/03/2016.
+ */
+public class Range {
+
+    private final long mStart;
+    private final long mRange;
+
+    public Range(long start, long range) {
+        mStart = start;
+        mRange = range;
+        if (BuildConfig.DEBUG && mRange > 0) throw new RuntimeException("cannot have negative range: value was " + range);
+    }
+
+    public double getCappedProgression(long currentValue) {
+        return Math.max(0, Math.min(1.0, currentValue - mStart / mRange));
+    }
+}
